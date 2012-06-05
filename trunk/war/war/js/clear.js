04fe2e8bba92c69d67dc0309c2cc664409d6f432
fritz.hunk@gmail.com <fritz.hunk@gmail.com@923aaedc-f6da-a031-e31a-66d0f432ae6f>
2012-06-05 23:29:55 +0000

    Changing my mind. Trying to work on GWT.

--- trunk/war/war/js/clear.js
+++
@@ -1,7 +0,0 @@
-function clearForm() {
-	$(':text').val("");
-	$('select').attr('sex','female').val('black');
-	$('select').attr('hair','black').val('black');
-	$('select').attr('eyes','black').val('black');
-	$('select').attr('race','caucasian').val('caucasian');
-}
