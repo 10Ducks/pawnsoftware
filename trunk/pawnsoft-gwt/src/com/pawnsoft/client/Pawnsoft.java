6eae9d9a0c3dde8c9a1a8ff5f504547d9401dc4f
fritz.hunk@gmail.com <fritz.hunk@gmail.com@923aaedc-f6da-a031-e31a-66d0f432ae6f>
2012-07-25 07:02:40 +0000

    Added some dummy files.

---
+++ trunk/pawnsoft-gwt/src/com/pawnsoft/client/Pawnsoft.java
@@ -0,0 +1,14 @@
+package com.pawnsoft.client;
+
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Pawnsoft implements EntryPoint {
+	public void onModuleLoad() {
+		Label helloWorld = new Label("Hello World");
+	}
+}
